b9d93b193fb6e567e33c5e29ee341ee2a4a34756
Egor Vasyakin <e.vasyakin@itevas.ru>
2023-05-02 02:33:47 +0300

    Test

---
+++ src/views/AboutView.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+
+    <CounterExample />
+  </div>
+</template>
+
+<script>
+import CounterExample from '@/components/CounterExample.vue'
+export default {
+  components: { CounterExample }
+}
+</script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <CounterExample />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+import CounterExample from '@/components/CounterExample.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    HelloWorld,
+    CounterExample
+  }
+}
+</script>
