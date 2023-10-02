7117b6e7ade98285853a789c63db84773788833a
Egor Vasyakin <e.vasyakin@itevas.ru>
2023-10-02 08:07:44 +0300

    Update Model.fields.display.js

--- src/core/Model/Model.fields.display.js
+++ src/core/Model/Model.fields.display.js
@@ -5,6 +5,7 @@
  * @license CC-BY-4.0
  */
 
+import { logger } from '../Log.js'
 import { Model } from './Model.js'
 
 /**
@@ -15,6 +16,11 @@ Model.setDisplayRules = function () {
     return {}
 }
 
+/**
+ * @var Правила переменного отображения полей
+ */
+Model.rulesForVariableDisplayOfFields = {}
+
 Model._displayRules = null
 
 /**
@@ -22,38 +28,29 @@ Model._displayRules = null
  * @param String|null имя поля
  * @return Object правила поля или полей
  */
-Model.displayRules = function (part = null) {
+Model.displayRules = function (group = null) {
     if (!this._displayRules) {
         const rules = this.setDisplayRules()
         // подтягиваем пропсы из полей
         Object.keys(rules).forEach(fieldName => {
             const field = this.field(fieldName)
-            if (field?.options) {
-                if ('string' === typeof rules[fieldName]) {
-                    rules[fieldName] = {
-                        component: rules[fieldName],
-                    }
-                }
-                if (!rules[fieldName].props) rules[fieldName].props = {}
-                rules[fieldName].props.options = field.options
+            if (!field) {
+                console.error(`Field for displayRule with name "${fieldName}" does not exists`)
+                return
             }
+            let fieldRules = rules[fieldName]
+            if (field?.options) {
+                if ('string' === typeof fieldRules) {
+                    fieldRules = { component: fieldRules }
+                }
+                if (!fieldRules.props) fieldRules.props = {}
+                fieldRules.props.options = field.options
+            }
+            field.setDisplay(fieldRules)
         })
         this._displayRules = rules
     }
-    console.log('displayRules', this._displayRules)
-    return part ? this._displayRules[part] : this._displayRules
-}
-
-/**
- * Получение правила отображения поля или самого поля.
- * @param String имя поля
- * @return Object|Field
- */
-Model.fieldView = function (name) {
-    return this.displayRules()[name] || this.field(name)
-}
-Model.prototype.$fieldView = function (name) {
-    return this.constructor.fieldView(name)
+    return [null, undefined].includes(group) ? this._displayRules : this._displayRules[group]
 }
 
 /**
@@ -65,37 +62,12 @@ Model.fieldNamesInDisplayRules = function () {
 }
 
 /**
- * Итеративная обработка отображений полей в функции.
- * @param Function функция обработки
- * @param Array|null имена полей
- * @return Boolean
- */
-Model.eachView = function (cb, names) {
-    if (!names) names = this.viewFieldNames()
-    for (let name of names) {
-        let view = this.fieldView(name)
-        if (!view) {
-            throw new Error(
-                `View for field "${name}" not registered in model "${this.entityName}"`
-            )
-        }
-        if (cb.apply(this, [view, name])) return true
-    }
-    return false
-}
-
-/**
- * @var Правила переменного отображения полей
- */
-Model.rulesForVariableDisplayOfFields = {}
-
-/**
  * Применение правил отображение полей в зависимости от значения других полей.
- * @param String|null имя поля
+ * @param String|Number|null отображаемая группа полей
  * @return Array поля доступные к отображению
  */
-Model.prototype.$applyFieldsDisplayRules = function (part = null) {
-    return Object.keys(this.constructor.displayRules(part)).reduce((viewFields, fieldName) => {
+Model.prototype.$applyFieldsDisplayRules = function (group = null) {
+    return Object.keys(this.constructor.displayRules(group)).reduce((viewFields, fieldName) => {
         const rule = this.constructor.rulesForVariableDisplayOfFields?.[fieldName]
         if (rule) {
             if (Array.isArray(rule)) {
@@ -107,7 +79,6 @@ Model.prototype.$applyFieldsDisplayRules = function (part = null) {
             } else if ('function' === typeof rule) {
                 rule(this) && viewFields.push(fieldName)
             }
-            console.log('rule', fieldName, rule, viewFields)
         } else {
             viewFields.push(fieldName)
         }
@@ -115,14 +86,71 @@ Model.prototype.$applyFieldsDisplayRules = function (part = null) {
     }, [])
 }
 
-Model.prototype.$fieldsToDisplay = function (part = null) {
-    return this.$applyFieldsDisplayRules(part).map(fieldName => {
-        return {
-            key: fieldName,
-            value: this[fieldName],
-            view: this.$fieldView(fieldName) || 'string',
-            isDirtyField: this.$isDirtyField(fieldName) || false,
-            disabled: false,
-        }
+/** @var String|Number|null отображаемая группа полей */
+Model.prototype.displayGroup = null
+
+/**
+ * Получение полей для отображения.
+ * @param String|Number|null отображаемая группа полей
+ * @return Array поля доступные к отображению
+ */
+Model.prototype.$displayFields = function (group = null) {
+    return logger.methodCall(`${this.$entityName}{${this.$id}}.$displayFields`, arguments, () => {
+        this.displayGroup = group
+        const displayFields = this.$applyFieldsDisplayRules(group)
+        logger.keyValue('$displayFields', displayFields)
+        return displayFields
     })
 }
+
+Model.prototype.$displayFieldsWithPrepare = function (cb = null, group = null) {
+    return this.$displayFields(group).map(fieldName => {
+        const fieldProps = {
+            fieldName: fieldName,
+            value: this[fieldName],
+            view: this.$field(fieldName),
+            isDirty: this.$isDirtyField(fieldName) || false,
+        }
+        return cb ? cb(fieldProps) : fieldProps
+    })
+}
+
+
+// /**
+//  * Получение правила отображения поля или самого поля.
+//  * @param String имя поля
+//  * @return Object|Field
+//  */
+// Model.fieldView = function (name, part = null) {
+//     return this.displayRules(part)[name] || this.field(name)
+// }
+// Model.prototype.$fieldView = function (name, part = null) {
+//     return this.constructor.fieldView(name, part)
+// }
+
+
+
+// Model.prototype.$fieldsToDisplay = function (part = null) {
+//     return this.$applyFieldsDisplayRules(part).map(fieldName => {
+//         return {
+//             key: fieldName,
+//             value: this[fieldName],
+//             view: this.$fieldView(fieldName) || 'string',
+//             isDirtyField: this.$isDirtyField(fieldName) || false,
+//             disabled: false,
+//         }
+//     })
+// }
+
+// Model.prototype.$fieldsToDisplay = function (part = null, cb = null) {
+//     const rules = this.$applyFieldsDisplayRules(part)
+//     return rules.map(fieldName => {
+//         const fieldProps = {
+//             fieldName: fieldName,
+//             value: this[fieldName],
+//             view: this.$fieldView(fieldName, part) || 'string',
+//             isDirty: this.$isDirtyField(fieldName) || false,
+//         }
+//         return cb ? cb(fieldProps) : fieldProps
+//     })
+// }
